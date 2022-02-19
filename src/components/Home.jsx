b4a8fe017828940cc5efe6c29278cfd750edb538
saquib <saquibali534@gmail.com>
2022-02-19 20:04:01 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import '../App.css';
+import Cards from './Cards';
+
+const Home = () => {
+
+    return (
+      <div>
+      <div className="main-name">
+      <p>Hello, Saquib!</p>
+      </div>
+        <div className="home">
+        <Cards color="#3E497A" tchrName="Sheldon" subject="DBMS"/>
+        <Cards color="#B8405E" tchrName="Leonard" subject="OS Lab"/>
+        <Cards color="#219F94" tchrName="Penny" subject="DSA"/>
+        <Cards color="#86C6F4" tchrName="Ammy" subject="DBMS"/>
+        <Cards color="#1572A1" tchrName="Raj" subject="Computer"/>
+        <Cards color="#781C68" tchrName="Howard" subject="DBMS"/>
+        <Cards color="#570530" tchrName="Bernadette" subject="DBMS"/>
+        <Cards color="#B8405E" tchrName="Stuart" subject="DBMS"/>
+        </div>
+        </div>
+      );
+
+}
+
+
+export default Home;
